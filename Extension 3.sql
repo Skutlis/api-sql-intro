995e2d2687351531991f9739c8b4d337a5d2fe3d
Erlend Skutlaberg <skutlis98@gmail.com>
2025-01-22 13:57:47 +0100

    Finished all exercises

--- Extension 3.sql
+++ Extension 3.sql
@@ -1,11 +1,4 @@
-CREATE view directorNfilms1 AS
-SELECT title, name from films_ext f 
+SELECT ARRAY[CONCAT(name, ':', CAST(COUNT(name) AS CHAR))] from films_ext f 
 INNER JOIN directors d
 ON f.directorId = d.id
 GROUP BY d.name, f.title;
-
-
-SELECT * FROM directorNfilms1;
-SELECT ARRAY[CONCAT(name, CAST(COUNT(name) AS CHAR))] FROM directorNfilms1;
-
-
